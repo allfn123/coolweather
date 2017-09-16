ae6c8f8342267b3bfb24e029ba7e2d3e072642d6
allfn123 <496169334@qq.com>
2017-09-16 22:36:46 +0800

    finish county select & weather display bascilly

--- app/src/main/java/com/example/administrator/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/administrator/coolweather/ChooseAreaFragment.java
@@ -10,6 +10,7 @@ package com.example.administrator.coolweather;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -91,6 +92,13 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                } else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Log.d("Weather",weatherId);
+                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
